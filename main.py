445b3eb3e5a3c741754b456e645c816a60a01676
DeD0k007 <ochkasov.alex@gmail.com>
2021-11-29 18:46:38 +0300

    init PhoneBook class

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 class PhoneContact:
-    name = "Alex"
-    contactNumber = 915
-    note = "note"
 
     def __init__(self):
+        self.name = "Alex"
+        self.contactNumber = 915
+        self.note = "note"
         self.result = str(self.name) + str(self.contactNumber) + str(self.note)
+
+
+class PhoneBook:
+
+    def __init__(self):
+        self.contactNumber = {"Alex": 915}
+
+    def getContactByName(self, name):
+        return self.contactNumber.get(name)
+
+    def postNewContact(self, name, contactNumber):
+        self.contactNumber[name] = contactNumber
