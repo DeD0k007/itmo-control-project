15c1d0a5d889186ae3069619d60c28b2d4d1cf6a
DeD0k007 <ochkasov.alex@gmail.com>
2021-11-30 19:35:27 +0300

    added database

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import config
+import sqlite3
+
+database = config.PATH_TO_DATABASE
+
+
 class PhoneContact:
 
     def __init__(self):
@@ -9,17 +15,28 @@ class PhoneContact:
 
 class PhoneBook:
 
-    def __init__(self):
-        self.contactNumber = {"Alex": 915}
+    def __init__(self, database_file):
+        self.connection = sqlite3.connect(database_file, check_same_thread=False)
+        self.connection.row_factory = sqlite3.Row
+        self.cursor = self.connection.cursor()
 
     def getContactByName(self, name):
-        res = self.contactNumber.get(name)
+        _SQL = """SELECT * FROM contacts
+                            WHERE name = ?"""
+        res = self.cursor.execute(_SQL, (name,)).fetchone()
         if not res:
             res = "Кажется такого человека нет в базе."
-        return res
+        return res["number"]
 
-    def postNewContact(self, name, contactNumber):
-        try:
-            self.contactNumber[str(name)] = int(contactNumber)
-        except ValueError:
-            self.contactNumber[str(name)] = 123
+    def postNewContact(self, name, contactNumber, note="No"):
+        _SQL = """INSERT INTO contacts
+                            (name, number, note)
+                            VALUES
+                            (?, ?, ?)"""
+        self.cursor.execute(_SQL, (name, contactNumber, note))
+        self.connection.commit()
+
+    def __del__(self):
+        self.connection.commit()
+        self.cursor.close()
+        self.connection.close()
