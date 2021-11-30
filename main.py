aa7c52800f8c8e9c909622b84f78f915652d2245
DeD0k007 <ochkasov.alex@gmail.com>
2021-11-30 19:50:44 +0300

    telebot

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import config
 import sqlite3
+import telebot
+
+API_KEY = config.API_KEY
+
+bot = telebot.TeleBot(API_KEY)
+
 
 database = config.PATH_TO_DATABASE
 
@@ -21,12 +27,15 @@ class PhoneBook:
         self.cursor = self.connection.cursor()
 
     def getContactByName(self, name):
-        _SQL = """SELECT * FROM contacts
-                            WHERE name = ?"""
-        res = self.cursor.execute(_SQL, (name,)).fetchone()
-        if not res:
-            res = "Кажется такого человека нет в базе."
-        return res["number"]
+        try:
+            _SQL = """SELECT * FROM contacts
+                                WHERE name = ?"""
+            res = self.cursor.execute(_SQL, (name,)).fetchone()
+            if not res:
+                res = "Кажется такого человека нет в базе."
+            return res["number"]
+        except Exception:
+            return "Такого человека нет в базе."
 
     def postNewContact(self, name, contactNumber, note="No"):
         _SQL = """INSERT INTO contacts
@@ -40,3 +49,42 @@ class PhoneBook:
         self.connection.commit()
         self.cursor.close()
         self.connection.close()
+
+
+dbConnecting = PhoneBook(database)
+
+
+def get_person(message):
+    name = message.text
+    res = dbConnecting.getContactByName(name)
+    bot.send_message(message.chat.id, str(res))
+
+
+def add_person(message, name):
+    number = message.text
+    dbConnecting.postNewContact(name, number)
+    bot.send_message(message.chat.id, "Ok")
+
+
+def add_person_name(message):
+    name = message.text
+    text = 'Введите номер человека'
+    sent = bot.send_message(message.chat.id, text)
+    bot.register_next_step_handler(sent, add_person, name)
+
+
+@bot.message_handler(commands=['add_person'])
+def add(message):
+    text = 'Введите имя человека'
+    sent = bot.send_message(message.chat.id, text)
+    bot.register_next_step_handler(sent, add_person_name)
+
+
+@bot.message_handler(commands=['get'])
+def get(message):
+    text = 'Введите имя человека'
+    sent = bot.send_message(message.chat.id, text)
+    bot.register_next_step_handler(sent, get_person)
+
+
+bot.polling(none_stop=True)
